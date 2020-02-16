1508be81d8a69114cbe18916741b66baeef26032
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-02-16 18:26:20 +0900

    index
    (javascript)
    - resize할때마다 화면 height구하기
    - 동그라미Btn 클릭시 해당 포폴위치로 스크롤.
    - topBtn, 포폴nav 스크롤시 나타나게함.
    - topBtn, scrollBtn 애니메이션 효과줌.

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 
 let htmlEl = document.querySelector('html');
+let portNav = document.querySelector('.port_nav');
 let main = document.querySelector('main')
 let intro = document.querySelector('.intro');
 let introTitle = intro.querySelector('.title_box');
@@ -8,17 +9,110 @@ let scrollBtn = intro.querySelector('.scrollBtn');
 let aboutMe = intro.querySelector('.aboutMe');
 let portFirst = document.querySelector('.portListBox:first-child');
 let footer = document.querySelector('footer');
+let topBtn = document.querySelector('.top_btn');
 
-//페이지 접속하면 intro 글자 나오게함.
-let introText = introTitle.querySelectorAll('span');
-console.log(introText);
-for(let i = 0; i < introText.length; i++) {
-    console.log(introText[i].innerText);
-    console.log(introText[i].innerText.toString());
-    console.log(introText[i].value);
+
+// ******resize 체크.******
+let portfolio = document.querySelector('.portfolio');
+let portListBox = document.querySelectorAll('.portListBox');
+let contentHeight;
+resize();
+function resize() {
+    let checksize;  
+
+    resizeCheck();
+
+    window.addEventListener('resize', resizeWork);
+
+    function resizeWork() {
+        clearTimeout(checksize);
+        checksize = setTimeout(function() {
+            resizeCheck();
+        }, 100);
+    }
+
+    function resizeCheck() {
+        contentHeight = `${window.innerHeight}`;
+
+        for(let i = 0; i < portListBox.length; i++) {
+            portListBox[i].style.height = contentHeight;
+        }
+
+        console.log(contentHeight);
+        
+    }
+}
+console.log(portfolio.offsetTop);
+
+// ******resize 체크.끝******
+
+
+// //페이지 접속하면 intro 글자 나오게함.
+// let introText = introTitle.querySelectorAll('span');
+// console.log(introText);
+// for(let i = 0; i < introText.length; i++) {
+//     console.log(introText[i].innerText);
+//     console.log(introText[i].innerText.toString());
+//     console.log(introText[i].value);
+// }
+
+
+// //scrollBtn 클릭시 포폴첫번쨰(kakao)로 스크롤.
+// let scrollBtnSpan = intro.querySelectorAll('.scrollBtn  span');
+// for(let i = 0; i < scrollBtnSpan.length; i++) {
+//     scrollBtnSpan[i].addEventListener('click', function(e) {
+//         console.log(e.target.tagName);
+        
+//         window.scrollTo(0, 100);
+//     }); 
+// }
+
+
+
+//port_nav 버튼 클릭시 해당 포폴 위치로 스크롤되게함.
+let portNavBtn = document.querySelectorAll('.port_nav > button');
+for(let i = 0; i < portNavBtn.length; i++) {
+    portNavBtn[i].addEventListener('click', function() {
+        window.scrollTo({          
+            top: contentHeight * (i + 1) + (110 * i),
+            behavior: 'smooth',
+        });
+        console.log(contentHeight * (i + 1));      
+    });
 }
 
+for(let i = 0; i < portListBox.length; i++) {
+   window.addEventListener('scroll', function() {
+       console.log(window.scrollY);
+       console.log(document.body.clientHeight);
+       //해당 포폴위치로 스크롤 될 때, port_nav버튼에 배경색 채워줌.
+       //만약 스크롤한 위치가 포폴한부분 안에 속해 있디면 색을 줘라
+       //전체길이 스크롤한값 
+       // 한부분의바닥값 = 전체길이 - (100vh높이(=contentHeight) * (내순서 + 1)) 
+       // 한부분의top값 = 전체길이 - (내순서 + 1)
+       // top값 < portListBox[i] 하나당 높이범위는 < 바닥값
+       let bottom = document.body.clientHeight - (contentHeight * (portListBox[i] + 1));
+       let top = document.body.clientHeightt - (portListBox[i] + 1);
+       if(top[i] < document.body.scrollTop < bottom[i]) {
+           for(let i = 0; i < portNavBtn.length; i++) {
+                portNavBtn[i].style.backgroundColor = '#000';
+            }
+       }
+        
 
+        // if(window.scrollY === portListBox[i].scrollTop) {
+        //    window.scrollBy(0, contentHeight);        
+        // }
+        
+    }); 
+}
+
+//해당 포폴위치로 스크롤 될 때, port_nav버튼에 배경색 채워줌.
+window.addEventListener('scroll', function() {
+    if(window) {
+        
+    }
+});
 
 
 //intro에서 마우스휠하면 intro의 title 스타일 변경되게(아래로 스트롤은 안됌)
@@ -46,7 +140,18 @@ intro.addEventListener('mousewheel', function(e) {
             scrollBtn.style.bottom = '30px'; 
             
         },1700);
-    } 
+    } else if(e.wheelDelta < 0 && main.style.width === '100%') {
+        window.scrollTo({
+            top: contentHeight,
+            behavior: 'smooth',
+        });
+        setTimeout(function() {
+            portNav.style.opacity = '1';
+            portNav.style.bottom = '-50px';
+            topBtn.style.opacity = '1';
+            topBtn.style.bottom = '30px';
+        }, 1500)
+    }
     else if(e.wheelDelta > 0 && intro.scrollTop >= 0) {
         scrollBtn.style.opacity = '0';       
         scrollBtn.style.bottom= '35px';
@@ -74,6 +179,7 @@ intro.addEventListener('mousewheel', function(e) {
 // portfolio중 첫번째에서 스크롤위로 올리면 intro의 title 스타일 원래대로 돌아오게.                   
 portFirst.addEventListener('mousewheel', function(e) {
     console.log(e.wheelDelta);
+    console.log(portFirst.scrollTop);
     
     if(portFirst.scrollTop === 0 && e.wheelDelta > 3) {
         window.scrollTo({
@@ -85,26 +191,30 @@ portFirst.addEventListener('mousewheel', function(e) {
 });
 //topBtn버튼에서도 써야해서 따로 선언해줌.
 let introReset = function() {
+    portNav.style.opacity = '0';
+    portNav.style.bottom = '-20px';
     scrollBtn.style.opacity = '0';       
-        scrollBtn.style.bottom= '35px'; 
-        aboutMe.style.opacity = '0'; 
-        aboutMe.style.right = '-10%'; 
-        setTimeout(function() {
-            introTitle.style.width = '50%';
-            introTitleText.style.left = '50%';
-        },1300);
-        setTimeout(function() {
-            main.style.width = 'calc(100% + -110px)'; 
-            main.style.margin = '0 55px'; 
-            scrollBtn.style.color = '#1c1b20'; 
-            scrollBtn.style.left = 'calc(50% + -30px)'; 
-            footer.style.height = '55px';  
-        },1700);
-        setTimeout(function() {          
-            main.style.position = 'fixed';
-            scrollBtn.style.opacity = '1'; 
-            scrollBtn.style.bottom = '60px'; 
-        },2100);
+    scrollBtn.style.bottom= '35px'; 
+    aboutMe.style.opacity = '0'; 
+    aboutMe.style.right = '-10%'; 
+    topBtn.style.opacity = '0';
+    topBtn.style.bottom = '0';
+    setTimeout(function() {
+        introTitle.style.width = '50%';
+        introTitleText.style.left = '50%';
+    },1300);
+    setTimeout(function() {
+        main.style.width = 'calc(100% + -110px)'; 
+        main.style.margin = '0 55px'; 
+        scrollBtn.style.color = '#1c1b20'; 
+        scrollBtn.style.left = 'calc(50% + -30px)'; 
+        footer.style.height = '55px';  
+    },1700);
+    setTimeout(function() {          
+        main.style.position = 'fixed';
+        scrollBtn.style.opacity = '1'; 
+        scrollBtn.style.bottom = '60px'; 
+    },2100);
 }
 
 
@@ -127,8 +237,6 @@ for(let i = 0; i < imgTitle.length; i++) {
 
 
 // top_btn클릭시 화면 상단으로 스크롤되게 함.
-let topBtn = document.querySelector('.top_btn');
-
 topBtn.addEventListener('click', function(e) { 
     e.preventDefault();
     window.scrollTo({
@@ -139,7 +247,7 @@ topBtn.addEventListener('click', function(e) {
 });
 
 
-//scrollBtn, topBtn 화살표 계속 움직이게하기.
+
 
 
 
