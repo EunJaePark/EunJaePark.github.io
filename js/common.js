1f617d83d7b0d9132c85b448dc7a4e4bd621bbd9
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-02-18 15:50:41 +0900

    index
    - phone화면까지 반응형 제작
    - pc, phone용 스크립트 따로 제작중.

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 
 let htmlEl = document.querySelector('html');
 let portNav = document.querySelector('.port_nav');
+let navBtn = document.querySelectorAll('nav button');
 let main = document.querySelector('main')
 let intro = document.querySelector('.intro');
 let introTitle = intro.querySelector('.title_box');
@@ -47,103 +48,65 @@ console.log(portfolio.offsetTop);
 // ******resize끝******
 
 
-// //페이지 접속하면 intro 글자 나오게함.
-// let introText = introTitle.querySelectorAll('span');
-// console.log(introText);
-// for(let i = 0; i < introText.length; i++) {
-//     console.log(introText[i].innerText);
-//     console.log(introText[i].innerText.toString());
-//     console.log(introText[i].value);
-// }
+// ******현재스크롤위치******
+function getScrollXY() {
+    var scrOfX = 0, scrOfY = 0;
+    if( typeof( window.pageYOffset ) == 'number' ) {
+    //Netscape compliant
+    scrOfY = window.pageYOffset;
+    scrOfX = window.pageXOffset;
+    } else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
+    //DOM compliant
+    scrOfY = document.body.scrollTop;
+    scrOfX = document.body.scrollLeft;
+    } else if( document.documentElement && ( document.documentElement.scrollLeft || document.documentElement.scrollTop ) ) {
+    //IE6 standards compliant mode
+    scrOfY = document.documentElement.scrollTop;
+    scrOfX = document.documentElement.scrollLeft;
+    }    
+    return { x:scrOfX, y:scrOfY };
+}    
+var currentScroll = getScrollXY();
+currentScroll.x // X 좌표
+currentScroll.y // Y 좌표
+// ******현재스크롤위치 끝.******
 
 
-// //scrollBtn 클릭시 포폴첫번쨰(kakao)로 스크롤.
-// let scrollBtnSpan = intro.querySelectorAll('.scrollBtn  span');
-// for(let i = 0; i < scrollBtnSpan.length; i++) {
-//     scrollBtnSpan[i].addEventListener('click', function(e) {
-//         console.log(e.target.tagName);
-        
-//         window.scrollTo(0, 100);
-//     }); 
-// }
 
 
-//nav .intro, .portfolio클릭시 해당 위치로 스크롤.
-let navBtn = document.querySelectorAll('nav button');
-console.log(navBtn);
-
-for(let i = 0; i < navBtn.length; i++) {
-    navBtn[i].addEventListener('click', function(e) {
-        //.intro
-        if(e.target.classList.contains('introBtn')) {
-            console.log('intro');
-            console.log(e.target.num);
-            
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-            introReset();
-        //.portfolio
-        } else if(e.target.classList.contains('portBtn')) {
-            console.log('prot');
-            window.scrollTo({
-                top: contentHeight,
-                behavior: 'smooth'
-            });
-            console.log(document.body.scrollTop);
-            
-            if(document.body.scrollTop >= contentHeight) {
-                setTimeout(function() {
-                    portNav.style.opacity = '1';
-                    portNav.style.bottom = '-50px';
-                    topBtn.style.opacity = '1';
-                    topBtn.style.bottom = '30px';
-                }, 500)
-            }
-            //.portfolio부분에서 포폴Btn클릭해서 스크롤시 topBtn이랑 portBtn에 조건걸어줬는데 안됌...ㅠ..... intro에서 한번 스크롤 한 뒤에 눌럿을떄만 뜨게 하려고 하는 건데 조건걸면 안나옴. 조건 안걸면 처음 로딩창에서 페이지는 스크롤되지도 않는데 버튼만 나타남,.....
-        }
-            
-    });
-}
-
-
-//port_nav 버튼 클릭시 해당 포폴 위치로 스크롤되게함.
 let portNavBtn = document.querySelectorAll('.port_nav > button');
 for(let i = 0; i < portNavBtn.length; i++) {
+    //port_nav 버튼 클릭시 해당 포폴 위치로 스크롤되게함. + 클릭한 버튼 색채움.
     portNavBtn[i].addEventListener('click', function() {
         window.scrollTo({          
             top: contentHeight * (i + 1) + (110 * i),
             behavior: 'smooth',
         });
-        console.log(contentHeight * (i + 1));      
+        console.log(contentHeight * (i + 1));  
+        for(let i = 0; i < portNavBtn.length; i++) {
+            portNavBtn[i].style.backgroundColor = 'transparent';
+        }
+        portNavBtn[i].style.backgroundColor = '#000';    
     });
-}
 
-for(let i = 0; i < portListBox.length; i++) {
-   window.addEventListener('scroll', function() {
+    //해당 포폴위치로 스크롤 될 때, port_nav버튼에 배경색 채워줌.
+    window.addEventListener('scroll', function() {
        console.log(window.scrollY);
        console.log(document.body.clientHeight);
-       //해당 포폴위치로 스크롤 될 때, port_nav버튼에 배경색 채워줌.
-       //만약 스크롤한 위치가 포폴한부분 안에 속해 있디면 색을 줘라
-       //전체길이 스크롤한값 
-       // 한부분의바닥값 = 전체길이 - (100vh높이(=contentHeight) * (내순서 + 1)) 
-       // 한부분의top값 = 전체길이 - (내순서 + 1)
-       // top값 < portListBox[i] 하나당 높이범위는 < 바닥값
-       let bottom = document.body.clientHeight - (contentHeight * (portListBox[i] + 1));
-       let top = document.body.clientHeightt - (portListBox[i] + 1);
-       if(top[i] < document.body.scrollTop < bottom[i]) {
+
+       if(currentScroll.y > (contentHeight * [i])) {
            for(let i = 0; i < portNavBtn.length; i++) {
+                for(let i = 0; i < portNavBtn.length; i++) {
+                    portNavBtn[i].style.backgroundColor = 'transparent';
+                }
                 portNavBtn[i].style.backgroundColor = '#000';
             }
        }
-        
-
-        // if(window.scrollY === portListBox[i].scrollTop) {
-        //    window.scrollBy(0, contentHeight);        
-        // }
-        
     }); 
+}
+
+for(let i = 0; i < portListBox.length; i++) {
+   
 }
 
 
@@ -153,18 +116,24 @@ window.addEventListener('load', function() {
         pcMouseWheel()
     }
 });
-
+//895px 이하에서 적용.
+window.addEventListener('load', function() {
+    if(window.innerWidth < 895) {
+        phoneScript()
+    }
+});
+//resize시 895px이상, 이하별로 js 다르게 주기.
 window.addEventListener('resize', function() {
     if(window.innerWidth > 895) {
         console.log('895보다 크다##');
         pcMouseWheel()
-    }  else{
+    }  else {
         console.log('895보다 작아졌따!!!!!!!!!!!!!!!');
-        !pcMouseWheel();
+        phoneScript();
     }
 });
 
-pcMouseWheel();
+// pcMouseWheel();
 //intro에서 마우스휠하면 intro의 title 스타일 변경되게(아래로 스트롤은 안됌)
 function pcMouseWheel() {
     intro.addEventListener('mousewheel', function(e) {
@@ -226,8 +195,43 @@ function pcMouseWheel() {
             },1700);
         }                        
     });
-    
-     // portfolio중 첫번째에서 스크롤위로 올리면 intro의 title 스타일 원래대로 돌아오게.                
+
+    //nav .intro, .portfolio클릭시 해당 위치로 스크롤.
+    for(let i = 0; i < navBtn.length; i++) {
+        navBtn[i].addEventListener('click', function(e) {
+            //.intro
+            if(e.target.classList.contains('introBtn')) {
+                console.log('intro');
+                console.log(e.target.num);
+                
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+                introReset();
+            //.portfolio
+            } else if(e.target.classList.contains('portBtn')) {
+                console.log('prot');
+                window.scrollTo({
+                    top: contentHeight,
+                    behavior: 'smooth'
+                });
+                console.log(document.body.scrollTop);
+                
+                if(document.body.scrollTop >= contentHeight) {
+                    setTimeout(function() {
+                        portNav.style.opacity = '1';
+                        portNav.style.bottom = '-50px';
+                        topBtn.style.opacity = '1';
+                        topBtn.style.bottom = '30px';
+                    }, 500)
+                }
+                //.portfolio부분에서 포폴Btn클릭해서 스크롤시 topBtn이랑 portBtn에 조건걸어줬는데 안됌...ㅠ..... intro에서 한번 스크롤 한 뒤에 눌럿을떄만 뜨게 하려고 하는 건데 조건걸면 안나옴. 조건 안걸면 처음 로딩창에서 페이지는 스크롤되지도 않는데 버튼만 나타남,.....
+            }
+                
+        });
+    }
+    // portfolio중 첫번째에서 스크롤위로 올리면 intro의 title 스타일 원래대로 돌아오게.     
     portFirst.addEventListener('mousewheel', function(e) {
         console.log(e.wheelDelta);
         console.log(portFirst.scrollTop);
@@ -241,48 +245,97 @@ function pcMouseWheel() {
         }                     
     });
 
-//topBtn버튼에서도 써야해서 따로 선언해줌.
-let introReset = function() {
-    portNav.style.opacity = '0';
-    portNav.style.bottom = '-20px';
-    scrollBtn.style.opacity = '0';       
-    scrollBtn.style.bottom= '35px'; 
-    aboutMe.style.opacity = '0'; 
-    aboutMe.style.right = '-10%'; 
-    topBtn.style.opacity = '0';
-    topBtn.style.bottom = '0';
-    setTimeout(function() {
-        introTitle.style.width = '50%';
-        introTitleText.style.left = '50%';
-    },1300);
-    setTimeout(function() {
-        main.style.width = 'calc(100% + -110px)'; 
-        main.style.margin = '0 55px'; 
-        scrollBtn.style.color = '#1c1b20'; 
-        scrollBtn.style.left = 'calc(50% + -30px)'; 
-        footer.style.height = '55px';  
-    },1700);
-    setTimeout(function() {          
-        main.style.position = 'fixed';
-        scrollBtn.style.opacity = '1'; 
-        scrollBtn.style.bottom = '60px'; 
-    },2100);
-}
+    //topBtn버튼에서도 써야해서 따로 선언해줌.
+    let introReset = function() {
+        portNav.style.opacity = '0';
+        portNav.style.bottom = '-20px';
+        scrollBtn.style.opacity = '0';       
+        scrollBtn.style.bottom= '35px'; 
+        aboutMe.style.opacity = '0'; 
+        aboutMe.style.right = '-10%'; 
+        topBtn.style.opacity = '0';
+        topBtn.style.bottom = '0';
+        setTimeout(function() {
+            introTitle.style.width = '50%';
+            introTitleText.style.left = '50%';
+        },1300);
+        setTimeout(function() {
+            main.style.width = 'calc(100% + -110px)'; 
+            main.style.margin = '0 55px'; 
+            scrollBtn.style.color = '#1c1b20'; 
+            scrollBtn.style.left = 'calc(50% + -30px)'; 
+            footer.style.height = '55px';  
+        },1700);
+        setTimeout(function() {          
+            main.style.position = 'fixed';
+            scrollBtn.style.opacity = '1'; 
+            scrollBtn.style.bottom = '60px'; 
+        },2100);
+    }
 
 
-// top_btn클릭시 화면 상단으로 스크롤되게 함.
-topBtn.addEventListener('click', function(e) { 
-    e.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
+    // top_btn클릭시 화면 상단으로 스크롤되게 함.
+    topBtn.addEventListener('click', function(e) { 
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+        if(window.innerWidth > 895) {
+        introReset(); 
+        } 
     });
-    if(window.innerWidth > 895) {
-       introReset(); 
-    } 
-});
 
 } //pcMouseWheel() 끝.
+
+// phoneScript();
+function phoneScript() { 
+    window.addEventListener('scroll', function() {
+        //.portfolio부분 시작되면 top_btn보이게 함.  
+        if(currentScroll.y > contentHeight) {
+            topBtn.classList.add('show');
+        } else {
+            topBtn.classList.remove('show');
+        }
+
+        //.portfolio마지막 페이지 가면 .scrollBtn버튼 사라지게함.
+        if(currentScroll.y > (contentHeight * 3)) {
+            scrollBtn.classList.add('hide');
+        } else {
+            scrollBtn.classList.remove('hide');
+        }
+
+        //nav .intro, .portfolio클릭시 해당 위치로 스크롤.
+        for(let i = 0; i < navBtn.length; i++) {
+            navBtn[i].addEventListener('click', function(e) {
+                //.intro
+                if(e.target.classList.contains('introBtn')) {                  
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                //.portfolio
+                } else if(e.target.classList.contains('portBtn')) {
+                    window.scrollTo({
+                        top: contentHeight,
+                        behavior: 'smooth'
+                    });
+                }                  
+            });
+        }
+
+        //.top_btn 누르면 top으로 스크롤.
+        topBtn.addEventListener('click', function() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        });
+
+    });
+    
+} //phoneScript() 끝.
+
 
 //포폴 img, text에 마웃스오버시 img바뀌게함.
 let imgTitle = document.querySelectorAll('.imgTitle > a');
