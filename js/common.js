03bd64c8a690da0600fc54855ed798e6fb61b462
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-03-02 20:33:24 +0900

    index
    반응형 사이즈 수정.

--- js/common.js
+++ js/common.js
@@ -1,46 +1,69 @@
 
-// let htmlEl = document.querySelector('html');
-// let portNav = document.querySelector('.port_nav');
-// let navBtn = document.querySelectorAll('nav button');
+let htmlEl = document.querySelector('html');
+let portNav = document.querySelector('.port_nav');
+let portNavBtn = document.querySelectorAll('.port_nav > button');
+let navBtn = document.querySelectorAll('nav button');
 // let main = document.querySelector('main')
-// let intro = document.querySelector('.intro');
+let intro = document.querySelector('.intro');
 // let introTitle = intro.querySelector('.title_box');
 // let introTitleText = introTitle.querySelector('.title');
-// let scrollBtn = intro.querySelector('.scrollBtn');
+let scrollBtn = intro.querySelector('.scrollBtn');
 // let aboutMe = intro.querySelector('.aboutMe');
 // let portFirst = document.querySelector('.portListBox:first-child');
 // let footer = document.querySelector('footer');
-// let topBtn = document.querySelector('.top_btn');
+let topBtn = document.querySelector('.top_btn');
 
 
-// // ******(100vh일때)resize될때마다 height값 찾기.******
-// let portfolio = document.querySelector('.portfolio');
-// let portListBox = document.querySelectorAll('.portListBox');
-// let vhHeight;
-// resize();
-// function resize() {
-//     let sizeCheck;  
-//     resizeHeight();
+// ******(100vh일때)resize될때마다 height값 찾기.******
+let portfolio = document.querySelector('.portfolio');
+let portListBox = document.querySelectorAll('.portListBox');
+let vhHeight;
+resize();
+function resize() {
+    let sizeCheck;  
+    resizeHeight();
 
-//     window.addEventListener('resize', function resizeWork() {
-//         this.clearTimeout(sizeCheck);
-//         sizeCheck = setTimeout(function() {
-//             resizeHeight();
-//         }, 100);
-//     });
+    window.addEventListener('resize', function resizeWork() {
+        this.clearTimeout(sizeCheck);
+        sizeCheck = setTimeout(function() {
+            resizeHeight();
+        }, 100);
+    });
 
-//     function resizeHeight() {
-//         vhHeight = window.innerHeight;
-//         console.log(vhHeight); 
-//         //height이 800px 이하일 경우에는 800으로 vhHeight 정해줌.(portfolio내용이 뒤죽박죽되기 때문에 정해준것) 
-//         if(vhHeight < 800) {
-//             vhHeight = 800;
-//         }     
-//     }
-// }
-// console.log(portfolio.offsetTop);
+    function resizeHeight() {
+        vhHeight = window.innerHeight;
+        console.log(vhHeight); 
+        //height이 800px 이하일 경우에는 800으로 vhHeight 정해줌.(portfolio내용이 뒤죽박죽되기 때문에 정해준것) 
+        if(vhHeight < 800) {
+            vhHeight = 800;
+        }     
+    }
+}
+console.log(portfolio.offsetTop);
 
-// // ******resize끝******
+// ******resize끝******
+
+
+// .portfolio로 스크롤하면 header 고정되게 함.
+let header = document.querySelector('header');
+console.log(intro.clientHeight);
+
+window.addEventListener('scroll', function() {
+    let scrollValue = document.documentElement.scrollTop ;
+    if(scrollValue > intro.clientHeight) {
+        header.classList.add('fixed');
+        scrollBtn.classList.add('hide');
+        this.setTimeout(function(){
+            header.classList.add('fixed2');
+        },50);
+    } else if(scrollValue < intro.clientHeight) {
+        header.classList.remove('fixed2');
+        this.setTimeout(function(){
+        header.classList.remove('fixed');
+        scrollBtn.classList.remove('hide');
+        }, 300);
+    }
+});
 
 
 
@@ -64,94 +87,52 @@
 //     }
 // });
 
-// // pcMouseWheel();
-// function pcMouseWheel() {
-//     //intro에서 마우스휠하면 intro의 title 스타일 변경되게(아래로 스트롤은 안됌)
-//     intro.addEventListener('mousewheel', function(e) {
-//         if(e.wheelDelta < 0 && htmlEl.scrollTop === 0) {
-//             scrollStyle();
-//         } else if(e.wheelDelta < 0 && main.style.width === '100%') {
-//             window.scrollTo({
-//                 top: vhHeight,
-//                 behavior: 'smooth',
-//             });
-//             setTimeout(function() {
-//                 portNav.style.opacity = '1';
-//                 portNav.style.bottom = '-50px';
-//                 topBtn.style.opacity = '1';
-//                 topBtn.style.bottom = '30px';
-//             }, 1500)
-//         }
-//         else if(e.wheelDelta > 0 && intro.scrollTop >= 0) {
-//             scrollBtn.style.opacity = '0';       
-//             scrollBtn.style.bottom= '35px';
-//             aboutMe.style.opacity = '0'; 
-//             aboutMe.style.right = '-10%'; 
-//             setTimeout(function() {
-//                 introTitle.style.width = '50%';
-//                 introTitleText.style.left = '50%';
-//                 scrollBtn.style.color = '#1c1b20'; 
-//                 scrollBtn.style.left = 'calc(50% + -30px)'; 
-//             },500);
-//             setTimeout(function() {
-//                 main.style.width = 'calc(100% + -110px)'; 
-//                 main.style.margin = '0 55px'; 
-//                 scrollBtn.style.opacity = '1'; 
-//                 scrollBtn.style.bottom = '60px'; 
-//                 footer.style.height = '55px'; 
-//             },900);
-//             setTimeout(function() {          
-//                 main.style.position = 'fixed';                     
-//             },1700);
-//         }                        
-//     });
+pcMouseWheel();
+function pcMouseWheel() {
 
-//     //nav .intro, .portfolio클릭시 해당 위치로 스크롤.
-//     for(let i = 0; i < navBtn.length; i++) {
-//         navBtn[i].addEventListener('click', function(e) {
-//             //먼저, main의 position을 static으로 바꿔줘야함.
-//             main.style.position = 'static';
-//             //.intro
-//             if(e.target.classList.contains('introBtn')) {
-//                 console.log('intro');
-//                 console.log(e.target.num);
+    //nav .intro, .portfolio클릭시 해당 위치로 스크롤.
+    for(let i = 0; i < navBtn.length; i++) {
+        navBtn[i].addEventListener('click', function(e) {
+            //.intro
+            if(e.target.classList.contains('introBtn')) {
+                console.log('intro');
+                console.log(e.target.num);
                 
-//                 window.scrollTo({
-//                     top: 0,
-//                     behavior: 'smooth'
-//                 });
-//                 introReset();
-//             //.portfolio
-//             } else if(e.target.classList.contains('portBtn')) {
-//                 console.log('prot');
-//                 scrollStyle();
-//                 setTimeout(function() {
-//                     portNav.style.opacity = '1';
-//                     portNav.style.bottom = '-50px';
-//                     topBtn.style.opacity = '1';
-//                     topBtn.style.bottom = '30px';
-//                 }, 1000)
-//                 portNavBtn[0].style.backgroundColor = '#000';  
-//                 window.scrollTo({
-//                     top: vhHeight,
-//                     behavior: 'smooth'
-//                 });
-//                 console.log(document.body.scrollTop);
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+                // introReset();
+            //.portfolio
+            } else if(e.target.classList.contains('portBtn')) {
+                // scrollStyle();
+                setTimeout(function() {
+                    portNav.style.opacity = '1';
+                    portNav.style.bottom = '-50px';
+                    topBtn.style.opacity = '1';
+                    topBtn.style.bottom = '30px';
+                }, 1000)
+                portNavBtn[0].style.backgroundColor = '#000';  
+                window.scrollTo({
+                    top: vhHeight,
+                    behavior: 'smooth'
+                });
+                console.log(document.body.scrollTop);
                 
-//                 if(document.body.scrollTop >= vhHeight) {
-//                     setTimeout(function() {
-//                         portNav.style.opacity = '1';
-//                         portNav.style.bottom = '-50px';
-//                         topBtn.style.opacity = '1';
-//                         topBtn.style.bottom = '30px';
-//                     }, 500)
-//                 }
-//             }
+                if(document.body.scrollTop >= vhHeight) {
+                    setTimeout(function() {
+                        portNav.style.opacity = '1';
+                        portNav.style.bottom = '-50px';
+                        topBtn.style.opacity = '1';
+                        topBtn.style.bottom = '30px';
+                    }, 500)
+                }
+            }
                 
-//         });
-//     }
+        });
+    }
 
-//     let portNavBtn = document.querySelectorAll('.port_nav > button');
+    
 //     for(let i = 0; i < portNavBtn.length; i++) {
 //         //port_nav 버튼 클릭시 해당 포폴 위치로 스크롤되게함. + 클릭한 버튼 색채움.
 //         portNavBtn[i].addEventListener('click', function() {
@@ -289,61 +270,65 @@
 //         }
 //     });
     
-// } //pcMouseWheel() 끝.
+} //pcMouseWheel() 끝.
 
 
 
-// // phoneScript();
-// function phoneScript() { 
-//     window.addEventListener('scroll', function() {
-//         let scrollValue = document.documentElement.scrollTop || document.body.scrollTop;
+// phoneScript();
+function phoneScript() { 
+    window.addEventListener('scroll', function() {
+        let scrollValue = document.documentElement.scrollTop || document.body.scrollTop;
 
-//         //.portfolio부분 시작되면 top_btn보이게 함. {
-//         if(scrollValue > vhHeight) {
-//             topBtn.classList.add('show');
-//         } else {
-//             topBtn.classList.remove('show');
-//         }
+        //.portfolio부분 시작되면 top_btn보이게 함. {
+        if(scrollValue > vhHeight) {
+            topBtn.classList.add('show');
+        } else {
+            topBtn.classList.remove('show');
+        }
 
-//         //.portfolio마지막 페이지 가면 .scrollBtn버튼 사라지게함.
-//         if(scrollValue > (vhHeight * (portListBox.length - 0.5))) {
-//             scrollBtn.classList.add('hide');
-//         } else {
-//             scrollBtn.classList.remove('hide');
-//         }     
+        //.portfolio마지막 페이지 가면 .scrollBtn버튼 사라지게함.
+        if(scrollValue > (vhHeight * (portListBox.length - 0.5))) {
+            scrollBtn.classList.add('hide');
+        } else {
+            scrollBtn.classList.remove('hide');
+        }     
         
 
-//         //nav .intro, .portfolio클릭시 해당 위치로 스크롤.
-//         for(let i = 0; i < navBtn.length; i++) {
-//             navBtn[i].addEventListener('click', function(e) {
-//                 //.intro
-//                 if(e.target.classList.contains('introBtn')) {                  
-//                     window.scrollTo({
-//                         top: 0,
-//                         behavior: 'smooth'
-//                     });
-//                 //.portfolio
-//                 } else if(e.target.classList.contains('portBtn')) {
-//                     window.scrollTo({
-//                         top: vhHeight,
-//                         behavior: 'smooth'
-//                     });
-//                 }                  
-//             });
-//         }
+        //nav .intro, .portfolio클릭시 해당 위치로 스크롤.
+        for(let i = 0; i < navBtn.length; i++) {
+            navBtn[i].addEventListener('click', function(e) {
+                //.intro
+                if(e.target.classList.contains('introBtn')) {                  
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                //.portfolio
+                } else if(e.target.classList.contains('portBtn')) {
+                    window.scrollTo({
+                        top: vhHeight,
+                        behavior: 'smooth'
+                    });
+                }                  
+            });
+        }
 
-//         //.top_btn 누르면 top으로 스크롤.
-//         topBtn.addEventListener('click', function(e) {
-//             e.preventDefault();
-//             window.scrollTo({
-//                 top: 0,
-//                 behavior: 'smooth',
-//             });
-//         });
+        //.top_btn 누르면 top으로 스크롤.
+        topBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        });
 
-//     });
+    });
     
-// } //phoneScript() 끝.
+} //phoneScript() 끝.
+
+
+
+
 
 
 //포폴 img, text에 마웃스오버시 img바뀌게함.
